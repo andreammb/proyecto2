6795232a87047576afb957ebd79d174ad6b2a79f
Andrea <andrea.moralesbarrantes@gmail.com>
2024-08-08 23:03:32 -0400

    test

--- marketbuy/index.js
+++ marketbuy/index.js
@@ -25,6 +25,11 @@ const loaders = require("@medusajs/medusa/dist/loaders/index").default
         })
       )
 
+    app.get("/", (req, res) => res.send("Express on Vercel"));
+
+    app.listen(3000, () => console.log("Server ready on port 3000."));
+
+    module.exports = app;
       // Handle graceful shutdown
       const gracefulShutDown = () => {
         server
